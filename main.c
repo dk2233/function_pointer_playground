706aa28507e2fb2a5be711b3f4f9dd7f27e858ad
danielk <kucharski22daniel@gmail.com>
2018-05-24 15:24:07 +0200

    added more to array with different functions

--- main.c
+++ main.c
@@ -77,10 +77,24 @@ void function1(EnumType arg_enum, struct1_type Arg_struct)
     printf(" %s ",Arg_struct.tab);
 }
 
+
+void function2(EnumType2 arg_enum, struct2_type Arg_struct)
+{
+    printf(" \n warning this is function of second type \n");
+    printf(" 2222 %d <-< enum ",arg_enum );
+    printf(" %s ",Arg_struct.tab);
+}
+
 void functionWrapper(uint8_t arg_u8,struct1_type *arg_struct1, struct2_type *arg_struct2 )
 {
     if (NULL != arg_struct1)
+    {
         function1((EnumType)arg_u8, *arg_struct1);
+    }
+    else if (NULL != arg_struct2)
+    {
+        function2((EnumType2)arg_u8, *arg_struct2);
+    }
 }
 
 
@@ -91,17 +105,25 @@ struct {
     void (*functionWrapper_pointer)(uint8_t arg_u8,struct1_type *arg_struct1, struct2_type *arg_struct2  );
     
 } tab_iteracyjna[]={
-    {(uint8_t)FIRST_DATA, &(struct1_type){7U, 0U, (char *)"aaaa"},NULL, &functionWrapper}
+    {(uint8_t)FIRST_DATA, &(struct1_type){7U, 0U, (char *)"aaaa"},NULL, &functionWrapper},
+    {(uint8_t)SECOND_DATA,NULL, &(struct2_type){17U, 10U, (char *)"other aaaa"}, &functionWrapper},
+    {(uint8_t)TRZECI,NULL, &(struct2_type){123U, 230U, (char *)"jeszcze inne"}, &functionWrapper},
 };
 
 //((uint8_t) FIRST_DATA,
 
 int main(int argc, const char * argv[])
 {
+    //void (*func_p)(void)=&function1;
     // insert code here...
     printf("Hello, World!\n");
     
-    tab_iteracyjna[0].functionWrapper_pointer(tab_iteracyjna[0].DataInput, tab_iteracyjna[0].struct1_input, tab_iteracyjna[0].struct2_input);
+    for(uint8_t iter=0; iter < sizeof(tab_iteracyjna)/sizeof(tab_iteracyjna[0]); iter++)
+    {
+    
+        tab_iteracyjna[iter].functionWrapper_pointer(tab_iteracyjna[iter].DataInput, tab_iteracyjna[iter].struct1_input, tab_iteracyjna[iter].struct2_input);
+    }
+    //(*func_p)(
     
     return 0;
 }
